e0f6f031af2294a36a468bc16e292dcf277552f9
calebdfk <dkweteza@outlook.com>
2024-06-24 16:10:42 -0700

    update 1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,12 @@
 export default function taskBlock(trueOrFalse) {
-	let task = false;
-	const task2 = true;
+  const task = false;
+  const task2 = true;
 
-	if (trueOrFalse) {
-		let task = true;
-		const task2 = false;
-	}
-
-	return [task, task2];
+  if (trueOrFalse) {
+    /* eslint-disable */
+    task;
+    task2;
+  }
+  
+  return [task, task2];
 }
