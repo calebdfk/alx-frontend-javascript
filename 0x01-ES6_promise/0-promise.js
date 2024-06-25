ccaa66e45e7124f5935a02daa966da9553f13aa8
calebdfk <dkweteza@outlook.com>
2024-06-25 19:58:38 +0000

    0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,10 +1,8 @@
-export default function getRespondFromAPI() {
-	/*eslint-disable*/
-	return new Promise((resolve, reject) => {
-		if (true) {
-			resolve();
-		}else{
-			reject();
-		}
-	});
-}	
+function getFullResponseFromAPI(apiEndpoint) {
+  return new Promise((resolve, reject) => {
+    if (apiEndpoint) resolve({ status: 200, body: 'Success' });
+    reject(Error('Failed to fetch API'));
+  });
+}
+// Export the function for external use
+export default getFullResponseFromAPI;	
